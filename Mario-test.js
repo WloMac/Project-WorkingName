8f2e5e7cd619f323ce1f062e3fc8bf536ad3eb12
Mario <ruczkowski.m@gmail.com>
2023-02-01 21:03:46 +0000

    API entertiment update

--- Mario-test.js
+++ Mario-test.js
@@ -1,20 +1,20 @@
+// Concerts - Artists Events Tracker API Documentation
+// Access live, upcoming and past music events by location, venue and artist name.
+// https://rapidapi.com/s.mahmoud97/api/concerts-artists-events-tracker/
 
-// Music Gigs and Concerts Tracker API
-// Get the upcoming events from your favorite artists. Retrieve information like where they are going to be presented, with who, and more!
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3305c8d758msh96587b8a3306f7ep1d1bafjsn300995769472',
+		'X-RapidAPI-Host': 'concerts-artists-events-tracker.p.rapidapi.com'
+	}
+};
 
-// https://zylalabs.com/api-marketplace/data/music+gigs+and+concerts+tracker+api/94?
-
-let apiKey = '693|gonkN6b3hwr6jJS0IHGk945WurtKPCywRUmSsIsD';
-
-// GET CONCERTS BY LOCATION
-// Get a list of events in a determined city or location in a given time range. 
-
-let URL = 'https://zylalabs.com/api/94/music+gigs+and+concerts+tracker+api/149/get+concerts+by+location';
-
-fetch("https://zylalabs.com/api/94/music+gigs+and+concerts+tracker+api/149/get+concerts+by+location??name=Ibiza&minDate=2022-08-05&maxDate=2022-08-10&page=1")
-.then(response => response.json())
-.then(data => console.log(data))
+fetch('https://concerts-artists-events-tracker.p.rapidapi.com/location?name=Paris&minDate=2023-05-20&maxDate=2023-05-30&page=1', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+    
 
 
 
-                                                                            
