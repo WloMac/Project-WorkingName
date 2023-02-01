e18c67eec3459d82043d924db97abb9dc3336585
Mario <ruczkowski.m@gmail.com>
2023-02-01 22:07:52 +0000

    add location and date event to API response

--- Mario-test.js
+++ Mario-test.js
@@ -10,11 +10,28 @@ const options = {
 	}
 };
 
-fetch('https://concerts-artists-events-tracker.p.rapidapi.com/location?name=Paris&minDate=2023-05-20&maxDate=2023-05-30&page=1', options)
+fetch('https://concerts-artists-events-tracker.p.rapidapi.com/location?name=Paris&minDate=2023-05-20&maxDate=2023-05-21&page=1', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-    
+  .then(response => {
+    let data = response.data;
+    let html = '';
+    data.forEach(item => {
+      html += `
+        <div>
+          <h2>${item.name}</h2>
+          <p>Location: ${item.location.name}</p>
+          <p>Date: ${item.startDate}</p>
+        </div>
+      `;
+      
+    });
+    document.getElementById("root").innerHTML = html;
+    document.getElementById("location").innerHTML = locationName;
+document.getElementById("date").innerHTML = date;
+  })
+  .catch(err => console.error(err));
+
+
 
 
 
