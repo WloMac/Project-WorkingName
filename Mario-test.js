dae36b72ba94902968bb5627416c8fd0a18dc282
Mario <ruczkowski.m@gmail.com>
2023-02-03 20:20:06 +0000

    add mediaquery for Mario-test.css, add website links in Mario-test.js, style cards using flexbox in Mario-test.css

--- Mario-test.js
+++ Mario-test.js
@@ -16,29 +16,30 @@ fetch('https://concerts-artists-events-tracker.p.rapidapi.com/location?name=Pari
     console.log(response.data);
     let data = response.data;
     let html = '';
+   
     data.forEach(item => {
      
       html +=  `
         <div class="cards">
           <h2 class="name">${item.name}</h2>
           <img src=${item.image} alt="img" class="images">
-          <p class="countryName">Country: ${item.location.address.addressCountry}</p>
-          <p class="countryName">City: ${item.location.address.addressLocality}</p>
-          <p class="countryName">Street name: ${item.location.address.streetAddress}</p>
+          <p class="country">Country: ${item.location.address.addressCountry}</p>
+          <p class="city">City: ${item.location.address.addressLocality}</p>
+          <p class="streetname">Street name: ${item.location.address.streetAddress}</p>
           <p class="location">Postal code: ${item.location.address.postalCode}</p>
-          <p class="location">Event place: ${item.location.name}</p>
+          <p class="eventplace">Event place: ${item.location.name}</p>
           <p class="date">Date: ${item.startDate}</p>
           <p class="description">Description: ${item.description}</p>
           <p class="description">Event status: ${item.eventStatus}</p>
-          <p class="description">Website: ${item.location.sameAs}</p>
-
-          
+          <p>Website:<a class="description" href="${item.location.sameAs}">${item.location.sameAs}</a></p>
         </div>
       `;
+
     });
     document.getElementById("root").innerHTML = html;
     document.getElementById("location").innerHTML = locationName;
 document.getElementById("date").innerHTML = date;
+
 
 
 
