2161748c6fb0ac7f528625f6f9b1c556a297e4d7
Mario <ruczkowski.m@gmail.com>
2023-02-07 13:58:31 +0000

    add mediaquery

--- Mario-test.js
+++ Mario-test.js
@@ -2,35 +2,34 @@
 // Access live, upcoming and past music events by location, venue and artist name.
 // https://rapidapi.com/s.mahmoud97/api/concerts-artists-events-tracker/
 
-const options = {
+// API endpoint
+let options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '3305c8d758msh96587b8a3306f7ep1d1bafjsn300995769472',
 		'X-RapidAPI-Host': 'concerts-artists-events-tracker.p.rapidapi.com'
 	}
 };
-
+// Fetch method 
 fetch('https://concerts-artists-events-tracker.p.rapidapi.com/location?name=Paris&minDate=2023-05-11&maxDate=2023-05-12&page=1', options)
 	.then(response => response.json())
   .then(response => {
     console.log(response.data);
     let data = response.data;
     let html = '';
-   
     data.forEach(item => {
-     
       html +=  `
         <div class="cards">
-          <h2 class="name">${item.name}</h2>
+          
           <img src=${item.image} alt="img" class="images">
-          <p class="country">Country: ${item.location.address.addressCountry}</p>
+          <h2 class="name">${item.name}</h2>
           <p class="city">City: ${item.location.address.addressLocality}</p>
           <p class="streetname">Street name: ${item.location.address.streetAddress}</p>
           <p class="location">Postal code: ${item.location.address.postalCode}</p>
           <p class="eventplace">Event place: ${item.location.name}</p>
           <p class="date">Date: ${item.startDate}</p>
           <p class="description">Description: ${item.description}</p>
-          <p class="description">Event status: ${item.eventStatus}</p>
+         
           <p>Website:<a class="description" href="${item.location.sameAs}">${item.location.sameAs}</a></p>
         </div>
       `;
@@ -40,13 +39,37 @@ fetch('https://concerts-artists-events-tracker.p.rapidapi.com/location?name=Pari
     document.getElementById("location").innerHTML = locationName;
 document.getElementById("date").innerHTML = date;
 
-
-
-
   })
   .catch(err => console.error(err));
 
+let options2 = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3305c8d758msh96587b8a3306f7ep1d1bafjsn300995769472',
+		'X-RapidAPI-Host': '50k-radio-stations.p.rapidapi.com'
+	}
+};
 
+fetch('https://50k-radio-stations.p.rapidapi.com/get/channels?keyword=a&country_id=50&page=1', options2)
+	.then(response => response.json())
+	.then(response => 
+    console.log(response))
+    let data2 = response.data
+    let html = '';
+    data.forEach(item2 => {
+      html +=  `
+       <div class="cards2">
+          
+      <img src=${item2.image} alt="img" class="images"></div>`;
+    });
+
+
+
+
+
+
+    
+  
 
 //   $("#").on("click", function(event) {
 //   })
