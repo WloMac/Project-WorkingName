975c5ce9c2f215ac63d6ce62c2b639ac249a16bd
Mario <ruczkowski.m@gmail.com>
2023-02-07 22:06:12 +0000

    add background color for body element, add header, change event name color

--- Mario-test.js
+++ Mario-test.js
@@ -8,10 +8,8 @@ function ipLocate() {
 .then(function (jsonresponse) {
       console.log(jsonresponse)
       console.log(jsonresponse.city)    
-      let city = jsonresponse.city;    
-
+      let city = jsonresponse.city;
       events(city)
-      
 });
   
 }
@@ -28,7 +26,7 @@ let options = {
 	}
 };
 
-// Fetch method 
+// Fetch method return API entertainment data
 function events(city) {
   fetch(`https://concerts-artists-events-tracker.p.rapidapi.com/location?name=${city}&minDate=2023-05-11&maxDate=2023-05-12&page=1`, options)
 	.then(response => response.json())
@@ -38,22 +36,28 @@ function events(city) {
     let html = '';
     data.forEach(item => {
       html +=  `
-        <div class="cards">
-          
-          <img src=${item.image} alt="img" class="images">
-          <h2 class="name">${item.name}</h2>
-          <p class="city">City: ${item.location.address.addressLocality}</p>
-          <p class="streetname">Street name: ${item.location.address.streetAddress}</p>
-          <p class="location">Postal code: ${item.location.address.postalCode}</p>
-          <p class="eventplace">Event place: ${item.location.name}</p>
-          <p class="date">Date: ${item.startDate}</p>
-          <p class="description">Description: ${item.description}</p>
-         
-          <p>Website:<a class="description" href="${item.location.sameAs}">${item.location.sameAs}</a></p>
-        </div>
+     
+      <div class="cards">
+      <img src=${item.image} alt="img" class="images">
+      <h2 class="name">${item.name}</h2>
+      <p class="city">City: ${item.location.address.addressLocality}</p>
+      <p class="streetname">Street name: ${item.location.address.streetAddress}, ${item.location.address.postalCode}</p>
+      
+      <p class="eventplace">Event place: ${item.location.name}</p>
+      <p class="date">Date: ${item.startDate}</p>
+      <p class="description">Description: ${item.description}</p>
+      <p>Website:<a target="_blank" class="description" href="${item.location.sameAs}">${item.location.sameAs}</a></p>
+      </div>
       `;
-
+      
     });
+    
+      // let strDate = 'YYYY-MM-DDTHH:MM:SS';
+      // let date = new Date(strDate);
+      // console.log(date.toUTCString());
+
+   
+
     document.getElementById("root").innerHTML = html;
     document.getElementById("location").innerHTML = locationName;
 document.getElementById("date").innerHTML = date;
