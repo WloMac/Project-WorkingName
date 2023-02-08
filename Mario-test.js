30b72e21625a4922f1032c114acd9e88405b858f
Mario <ruczkowski.m@gmail.com>
2023-02-08 01:01:30 +0000

    Add bootstrap cards and buttons

--- Mario-test.js
+++ Mario-test.js
@@ -36,28 +36,18 @@ function events(city) {
     let html = '';
     data.forEach(item => {
       html +=  `
-     
-      <div class="cards">
+      <div class="card" style="width: 18rem;">
       <img src=${item.image} alt="img" class="images">
-      <h2 class="name">${item.name}</h2>
-      <p class="city">City: ${item.location.address.addressLocality}</p>
-      <p class="streetname">Street name: ${item.location.address.streetAddress}, ${item.location.address.postalCode}</p>
-      
-      <p class="eventplace">Event place: ${item.location.name}</p>
-      <p class="date">Date: ${item.startDate}</p>
-      <p class="description">Description: ${item.description}</p>
-      <p>Website:<a target="_blank" class="description" href="${item.location.sameAs}">${item.location.sameAs}</a></p>
+      <div class="card-body">
+        <h5 class="name">${item.name}</h5>
+        <p class="city">${item.location.address.addressLocality} - ${item.location.name}</p>
+        <p class="date">Date: ${item.startDate}</p>
+        <a href="${item.location.sameAs}" target="_blank" class="btn btn-outline-danger">More Info</a>
       </div>
+    </div>
       `;
-      
     });
     
-      // let strDate = 'YYYY-MM-DDTHH:MM:SS';
-      // let date = new Date(strDate);
-      // console.log(date.toUTCString());
-
-   
-
     document.getElementById("root").innerHTML = html;
     document.getElementById("location").innerHTML = locationName;
 document.getElementById("date").innerHTML = date;
@@ -67,45 +57,17 @@ document.getElementById("date").innerHTML = date;
 
 }
   
-  //Radio station API optional
 
-// let options2 = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '3305c8d758msh96587b8a3306f7ep1d1bafjsn300995769472',
-// 		'X-RapidAPI-Host': '50k-radio-stations.p.rapidapi.com'
-// 	}
-// };
+// <div class="cards">
+// <img src=${item.image} alt="img" class="images">
+// <h2 class="name">${item.name}</h2>
+// <p class="city">${item.location.address.addressLocality} - ${item.location.name}</p>
+// <p class="date">Date: ${item.startDate}</p>
 
-// fetch('https://50k-radio-stations.p.rapidapi.com/get/channels?keyword=a&country_id=50&page=1', options2)
-// 	.then(response => response.json())
-// 	.then(response => 
-//     console.log(response))
-//     let data2 = response.data
-//     let html = '';
-//     data.forEach(item2 => {
-//       html +=  `
-//        <div class="cards2">
-          
-//       <img src=${item2.image} alt="img" class="images"></div>`;
-//     });
+// <button class="website-button"><a target="_blank" class="description" href="${item.location.sameAs}">Buy tickets</a></button>
+// </div>
 
-
-
-
-
-
-    
-  
-
-//   $("#").on("click", function(event) {
-//   })
-
-// get IP from user 
-//   var userIP = $("#userIp").val();
-
-
-
+//<p class="description">Description: ${item.description}</p>
 
 
 
