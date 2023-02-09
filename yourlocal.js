bb95dbc6ad440e97498e04ab922f39c24b2f3177
ShaHuss <117936918+ShaHuss@users.noreply.github.com>
2023-02-09 19:15:28 +0000

    Header city and temp reworking with search history

--- yourlocal.js
+++ yourlocal.js
@@ -1,18 +1,64 @@
+let welcomeCity = document.getElementById("welcome");
+let searchedHistoryArray =
+  JSON.parse(localStorage.getItem("searchHistoryLocal")) || [];
 
-// Wlodek's Input - Location based on IP 
+// Wlodek's Input - Location based on IP
 ipLocate();
+render();
+function render() {
+  searchedHistoryArray =
+    JSON.parse(localStorage.getItem("searchHistoryLocal")) || [];
+  document.getElementById("history").innerHTML = "";
+
+  if (searchedHistoryArray.length < 1) {
+    for (i = 0; i < 6; i++) {
+      let historyText = "";
+      let displayText = $("<p>").text(historyText).attr("id", "newText");
+      $("#history").append(displayText);
+    }
+  } else {
+    for (i = 0; i < 5; i++) {
+      let historyText = searchedHistoryArray[i];
+      let displayText = $("<p>").text(historyText).attr("id", "newText");
+      $("#history").append(displayText);
+    }
+  }
+}
+
+function saveCityHistory(citySearchedFor) {
+  if (searchedHistoryArray.length > 5) {
+    searchedHistoryArray.unshift([citySearchedFor]);
+    searchedHistoryArray.pop;
+  } else {
+    searchedHistoryArray.unshift([citySearchedFor]);
+  }
+  localStorage.setItem(
+    "searchHistoryLocal",
+    JSON.stringify(searchedHistoryArray)
+  );
+}
+
+function displayBannerInfo(city) {
+  let welcomeMessage = "Welcome from " + city;
+  welcomeCity.innerHTML = welcomeMessage;
+}
 
 function ipLocate() {
-  fetch("https://ipgeolocation.abstractapi.com/v1/?api_key=5843dce2715140eca64c3152ccff5fb3")
-.then((response) => response.json())
-.then(function (jsonresponse) {
+  fetch(
+    "https://ipgeolocation.abstractapi.com/v1/?api_key=5843dce2715140eca64c3152ccff5fb3"
+  )
+    .then((response) => response.json())
+    .then(function (jsonresponse) {
       let lng = jsonresponse.longitude;
-      let lat= jsonresponse.latitude;    
+      let lat = jsonresponse.latitude;
       let city = jsonresponse.city;
-      events(city)
-      weather(lng, lat)
-});
+      events(city);
+      weather(lng, lat);
+      let welcomeMessage = "Welcome from " + city;
+      welcomeCity.innerHTML = welcomeMessage;
 
+      let todayWeatherMessage = "your temperature today is: ";
+    });
 }
 
 // Concerts - Artists Events Tracker API Documentation
@@ -20,125 +66,139 @@ function ipLocate() {
 // https://rapidapi.com/s.mahmoud97/api/concerts-artists-events-tracker/
 // Entertainment/Events API endpoint
 let options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '3305c8d758msh96587b8a3306f7ep1d1bafjsn300995769472',
-		'X-RapidAPI-Host': 'concerts-artists-events-tracker.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3305c8d758msh96587b8a3306f7ep1d1bafjsn300995769472",
+    "X-RapidAPI-Host": "concerts-artists-events-tracker.p.rapidapi.com",
+  },
 };
 
 // Fetch method return API entertainment data
- function events(city) {
-  
-  fetch(`https://concerts-artists-events-tracker.p.rapidapi.com/location?name=${city}&minDate=2023-02-09&maxDate=2023-02-10&page=1`, options)
-	.then(response => response.json())
-  .then(response => {
-    console.log(response.data);
-    let data = response.data;
-    let html = '';
-    data.forEach(item => {
-      html +=  `
+function events(city) {
+  fetch(
+    `https://concerts-artists-events-tracker.p.rapidapi.com/location?name=${city}&minDate=2023-02-09&maxDate=2023-02-10&page=1`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response.data);
+      let data = response.data;
+      let html = "";
+      data.forEach((item) => {
+        html += `
       
       <div class="card" style="width: 18rem;">
       <img src=${item.image} alt="img" class="images">
       <div class="card-body">
         <h5 class="name">${item.name}</h5>
-        <p class="city">${item.location.address.addressLocality} - ${item.location.name}</p>
-        <p class="date">Date: ${item.startDate.slice(0, -14)} ${item.startDate.slice(11, -8)} </p>
-        <a href="${item.location.sameAs}" target="_blank" class="btn btn-outline-danger">More Info</a>
+        <p class="city">${item.location.address.addressLocality} - ${
+          item.location.name
+        }</p>
+        <p class="date">Date: ${item.startDate.slice(
+          0,
+          -14
+        )} ${item.startDate.slice(11, -8)} </p>
+        <a href="${
+          item.location.sameAs
+        }" target="_blank" class="btn btn-outline-danger">More Info</a>
       </div>
     </div>
       `;
-    });
-    
-    document.getElementById("root").innerHTML = html;
-    
-  })
-  .catch(err => console.error(err));
-  
+      });
+
+      document.getElementById("root").innerHTML = html;
+    })
+    .catch((err) => console.error(err));
 }
 
-
-//Events displayed based on user city input 
+//Events displayed based on user city input
 let button = document.querySelector(".button-search");
 let inputValue = document.querySelector(".inputValue");
 
-button.addEventListener('click', function(){
-  fetch(`https://concerts-artists-events-tracker.p.rapidapi.com/location?name=${inputValue.value}&minDate=2023-02-09&maxDate=2023-02-10&page=1`, options)
-	.then(response => response.json())
-  .then(response => {
-    console.log(response.data);
-    let data = response.data;
-    let html = '';
-    data.forEach(item => {
-      html +=  `
+button.addEventListener("click", function () {
+  let citySearchedFor = inputValue.value;
+  saveCityHistory(citySearchedFor);
+  fetch(
+    `https://concerts-artists-events-tracker.p.rapidapi.com/location?name=${inputValue.value}&minDate=2023-02-09&maxDate=2023-02-10&page=1`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response.data);
+      let data = response.data;
+      let html = "";
+      data.forEach((item) => {
+        html += `
       
       <div class="card" style="width: 18rem;">
       <img src=${item.image} alt="img" class="images">
       <div class="card-body">
         <h5 class="name">${item.name}</h5>
-        <p class="city">${item.location.address.addressLocality} - ${item.location.name}</p>
-        <p class="date">Date: ${item.startDate.slice(0, -14)} ${item.startDate.slice(11, -8)} </p>
+        <p class="city">${item.location.address.addressLocality} - ${
+          item.location.name
+        }</p>
+        <p class="date">Date: ${item.startDate.slice(
+          0,
+          -14
+        )} ${item.startDate.slice(11, -8)} </p>
        
-        <a href="${item.location.sameAs}" target="_blank" class="btn btn-outline-danger">More Info</a>
+        <a href="${
+          item.location.sameAs
+        }" target="_blank" class="btn btn-outline-danger">More Info</a>
       </div>
     </div>
       `;
-    });
-    
-    document.getElementById("root").innerHTML = html;
-  })
-  .catch(err => console.error(err));
-  
+        let welcomeMessage = "Your chosen city is " + inputValue.value;
+        welcomeCity.innerHTML = welcomeMessage;
+        document.getElementById("todayWeather").innerHTML = "";
+       
+        render();
+      });
+
+      document.getElementById("root").innerHTML = html;
+    })
+    .catch((err) => console.error(err));
 });
 
-
-// ------------------------ Wlodek input ----------------------// 
+// ------------------------ Wlodek input ----------------------//
 
 function weather(lng, lat) {
-  fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&timezone=auto&daily=temperature_2m_max,precipitation_sum,windspeed_10m_max,sunrise,sunset`)
-	.then(response => response.json())
-  .then(response => {
-    console.log(response)
-    let htmlInput = '';
-    htmlInput = document.getElementById("todayWeather");
-    let temp = response.daily.temperature_2m_max
-    let todayDate = response.daily.time  
-    htmlInput.textContent = `Today is ${todayDate[0]},\n with highs of ${temp[0]} °C`
+  fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&timezone=auto&daily=temperature_2m_max,precipitation_sum,windspeed_10m_max,sunrise,sunset`
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      let htmlInput = "";
+      htmlInput = document.getElementById("todayWeather");
+      let temp = response.daily.temperature_2m_max;
+      let todayDate = response.daily.time;
+      htmlInput.textContent = `your temperature today is: "  ${temp[0]} °C`;
 
-    // let sunset = response.daily.sunset
-    // let sunrise = response.daily.sunrise  
-    // let wind = response.daily.windspeed_10m_max
-    // let forecastDate = response.daily.time
-    // let data = response.daily
-    // let html3 = '';
-    // Object.entries(data.sunset).forEach(item => {
-    //   console.log(item)
-    //   html3 +=  `
-    //     <div class="card" style="width: 18rem;>  
-    //       <div class="card-body">  
-    //         <h5 class="card-title date"> Date: ${forecastDate[item[0]]}</h5>      
-    //         <p class="card-text sunrise">Sunrise:${sunrise[item[0]]}</p>
-    //         <p class="card-text sunset">Sunset: ${sunset[item[0]]}</p>                 
-    //         <p class="card-text temp">Temp.:${temp[item[0]]}</p>
-    //         <p class="card-text wind">Wind: ${wind[item[0]]}</p>
-    //       </div>
-    //     </div>
-    //   `;
+      // let sunset = response.daily.sunset
+      // let sunrise = response.daily.sunrise
+      // let wind = response.daily.windspeed_10m_max
+      // let forecastDate = response.daily.time
+      // let data = response.daily
+      // let html3 = '';
+      // Object.entries(data.sunset).forEach(item => {
+      //   console.log(item)
+      //   html3 +=  `
+      //     <div class="card" style="width: 18rem;>
+      //       <div class="card-body">
+      //         <h5 class="card-title date"> Date: ${forecastDate[item[0]]}</h5>
+      //         <p class="card-text sunrise">Sunrise:${sunrise[item[0]]}</p>
+      //         <p class="card-text sunset">Sunset: ${sunset[item[0]]}</p>
+      //         <p class="card-text temp">Temp.:${temp[item[0]]}</p>
+      //         <p class="card-text wind">Wind: ${wind[item[0]]}</p>
+      //       </div>
+      //     </div>
+      //   `;
 
-    // });
-    // document.getElementById("root3").innerHTML = html3;
-
-    
-    
-    
-  })
-  
+      // });
+      // document.getElementById("root3").innerHTML = html3;
+    });
 }
-
-
-
-
 
 // <div class="cards">
 // <img src=${item.image} alt="img" class="images">
@@ -151,7 +211,6 @@ function weather(lng, lat) {
 
 //<p class="description">Description: ${item.description}</p>
 
-
 // Code copied from Wlodek-test.js -----------------------------------------------------------------------------------------------------
 
 // function news(city) {
@@ -163,20 +222,19 @@ function weather(lng, lat) {
 //     let html2 = '';
 //     data.forEach(item => {
 //       html2 +=  `
-//         <div class="cards">          
+//         <div class="cards">
 //           <h2 class="title">${item.title}</h2>
-//           <p class="date">Date: ${item.publishedAt}</p>                 
+//           <p class="date">Date: ${item.publishedAt}</p>
 //           <p>More:<a class="description" href="${item.url}">${item.url}</a></p>
 //         </div>
 //       `;
 
 //     });
 //     document.getElementById("root2").innerHTML = html2;
-  
+
 //   })
 //   .catch(err => console.error(err));
 
-  
 // function weather(lng, lat) {
 //   fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&timezone=auto&daily=temperature_2m_max,precipitation_sum,windspeed_10m_max,sunrise,sunset`)
 // 	.then(response => response.json())
@@ -194,11 +252,11 @@ function weather(lng, lat) {
 //     Object.entries(data.sunset).forEach(item => {
 //       console.log(item)
 //       html3 +=  `
-//         <div class="card" style="width: 18rem;>  
-//           <div class="card-body">  
-//             <h5 class="card-title date"> Date: ${forecastDate[item[0]]}</h5>      
+//         <div class="card" style="width: 18rem;>
+//           <div class="card-body">
+//             <h5 class="card-title date"> Date: ${forecastDate[item[0]]}</h5>
 //             <p class="card-text sunrise">Sunrise:${sunrise[item[0]]}</p>
-//             <p class="card-text sunset">Sunset: ${sunset[item[0]]}</p>                 
+//             <p class="card-text sunset">Sunset: ${sunset[item[0]]}</p>
 //             <p class="card-text temp">Temp.:${temp[item[0]]}</p>
 //             <p class="card-text wind">Wind: ${wind[item[0]]}</p>
 //           </div>
@@ -207,9 +265,7 @@ function weather(lng, lat) {
 
 //     });
 //     document.getElementById("root3").innerHTML = html3;
-    
+
 //   })
-  
+
 // }
-
-
